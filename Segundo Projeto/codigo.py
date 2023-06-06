9d556371d91f0781e7e15e046d9c8adcdf78d1cd
dioic3 <joyce.dias@gec.inatel.br>
2023-06-05 22:25:39 -0300

    adicionado os arquivos

--- Segundo Projeto/codigo.py
+++ Segundo Projeto/codigo.py
@@ -1,7 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('ruido2.jpg')
+img = cv2.imread('ruido2.jpeg')
 median = cv2.medianBlur(img,5)
  
 plt.subplot(121),plt.imshow(img),plt.title('Original')
