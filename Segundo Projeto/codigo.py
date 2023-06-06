06d50cdbd0aa7d9fac9651e72d17316a15c2b145
dioic3 <joyce.dias@gec.inatel.br>
2023-06-05 22:18:50 -0300

    adição do segundo projeto

---
+++ Segundo Projeto/codigo.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('ruido2.jpg')
+median = cv2.medianBlur(img,5)
+ 
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(median),plt.title('Mediana')
+plt.xticks([]), plt.yticks([])
+plt.show()
